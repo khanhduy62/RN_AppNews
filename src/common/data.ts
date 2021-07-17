b88dfd6ffce105ed61ddb481d70daa22c6cec20f
macbook <haihoang1993@gmail.com>
2021-07-17 09:15:29 +0700

    add category

---
+++ src/common/data.ts
@@ -0,0 +1,59 @@
+
+const categorys = [
+    {
+        id: 'top-stories',
+        name: 'Top Stories',
+    },
+    {
+        id: 'cryptocurrency',
+        name: 'Cryptocurrency',
+    },
+    {
+        id: 'world',
+        name: 'World',
+    },
+    {
+        id: 'africa',
+        name: 'Africa',
+    },
+    {
+        id: 'americas',
+        name: 'Americas',
+    },
+    {
+        id: 'asia',
+        name: 'Asia',
+    },
+    {
+        id: 'europe',
+        name: 'Europe',
+    },
+    {
+        id: 'middle-east',
+        name: 'Middle East',
+    },
+    {
+        id: 'u-s',
+        name: 'U.S.',
+    },
+    {
+        id: 'technology',
+        name: 'Technology',
+    },
+    {
+        id: 'science-space',
+        name: 'Science & Space',
+    },
+    {
+        id: 'entertainment',
+        name: 'Entertainment',
+    },
+    {
+        id: 'world-sport',
+        name: 'World Sport'
+    },
+]
+
+export {
+    categorys
+}
